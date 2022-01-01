136603d0558829b5170eb01e34351e1736846119
Äkwav <xekwavx@gmail.com>
2022-01-01 23:36:24 +0100

    more detailed profit data

--- Commands/Minecraft/OnlineCommand.cs
+++ Commands/Minecraft/OnlineCommand.cs
@@ -6,9 +6,11 @@ namespace Coflnet.Sky.Commands.MC
     {
         public override async Task Execute(MinecraftSocket socket, string arguments)
         {
-            socket.SendMessage(COFLNET + $"There are {McColorCodes.AQUA}{hypixel.FlipperService.Instance.PremiumUserCount}{McColorCodes.GRAY} users connected to this server");
+            socket.SendMessage(COFLNET + $"There are {McColorCodes.AQUA}{hypixel.FlipperService.Instance.PremiumUserCount}{McColorCodes.GRAY} users connected to this server", 
+                    null, McColorCodes.GRAY + "there is more than one server");
             var count = await Commands.FlipTrackingService.Instance.ActiveFlipperCount();
-            socket.SendMessage(COFLNET + $"{McColorCodes.AQUA}{count}{McColorCodes.GRAY} players clicked on a flip in the last 3 minutes.");
+            socket.SendMessage(COFLNET + $"{McColorCodes.AQUA}{count}{McColorCodes.GRAY} players clicked on a flip in the last 3 minutes.", 
+                    null, McColorCodes.GRAY + "accross all plans (free included)");
         }
     }
 }
--- Commands/Minecraft/ProfitCommand.cs
+++ Commands/Minecraft/ProfitCommand.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Coflnet.Sky.Commands.MC
@@ -7,24 +8,50 @@ namespace Coflnet.Sky.Commands.MC
     {
         public override async Task Execute(MinecraftSocket socket, string arguments)
         {
-            if(!double.TryParse(arguments.Trim('"'),out double days))
+            if (!double.TryParse(arguments.Trim('"'), out double days))
             {
-                socket.SendMessage(COFLNET + "usage /cofl profit {amountOfDays}");
+                socket.SendMessage(COFLNET + "usage /cofl profit {0.5-7}");
                 return;
             }
             var time = TimeSpan.FromDays(days);
-            if (time > TimeSpan.FromDays(2))
+            if (time > TimeSpan.FromDays(7))
             {
-                socket.SendMessage(COFLNET + "sorry the maximum is two days currently");
-                time = TimeSpan.FromDays(2);
+                socket.SendMessage(COFLNET + "sorry the maximum is a week currently");
+                time = TimeSpan.FromDays(7);
             }
 
             var response = await Sky.Commands.FlipTrackingService.Instance.GetPlayerFlips(socket.McUuid, time);
-
-            socket.SendMessage(COFLNET + $"According to our data you made {McColorCodes.AQUA}{socket.FormatPrice(response.TotalProfit)}{McColorCodes.GRAY} " 
+            var tfm = GetProfitForFinder(response, LowPricedAuction.FinderType.TFM);
+            var stonks = GetProfitForFinder(response, LowPricedAuction.FinderType.STONKS);
+            var other = GetProfitForFinder(response, LowPricedAuction.FinderType.EXTERNAL);
+            var coflnet = response.Flips.Where(f => f.Finder == LowPricedAuction.FinderType.FLIPPER 
+                                                || f.Finder == LowPricedAuction.FinderType.SNIPER 
+                                                || f.Finder == LowPricedAuction.FinderType.SNIPER_MEDIAN)
+                                                .Sum(f => f.SoldFor - f.PricePaid);
+            var word = response.TotalProfit > 10_000_000 ? "WOOHOOO" : "total of";
+            var hover = $"{word} {FormatPrice(socket, response.TotalProfit)} coins";
+            if (tfm > 0)
+                hover += $"\n {FormatPrice(socket, tfm)} from {McColorCodes.GOLD} TFM{McColorCodes.GRAY}";
+            if (stonks > 0)
+                hover += $"\n {FormatPrice(socket, stonks)} from Stonks";
+            if (other > 0)
+                hover += $"\n {FormatPrice(socket, other)} from other finders";
+            if (tfm > 0 || stonks > 0)
+                hover += $"\n {FormatPrice(socket, other)} from the {COFLNET} mod";
+            socket.SendMessage(COFLNET + $"According to our data you made {McColorCodes.AQUA}{socket.FormatPrice(response.TotalProfit)}{McColorCodes.GRAY} "
                 + $"in the last {McColorCodes.AQUA}{time.TotalDays}{McColorCodes.GRAY} days accross {McColorCodes.AQUA}{response.Flips.Length}{McColorCodes.GRAY} auctions",
-                null,
-                $"whohoo {McColorCodes.AQUA}{socket.FormatPrice(response.TotalProfit)}{McColorCodes.GRAY} coins");
+                null,hover);
         }
+
+        private static long GetProfitForFinder(Shared.FlipSumary response, LowPricedAuction.FinderType type)
+        {
+            return response.Flips.Where(f => f.Finder == type).Sum(f => f.SoldFor - f.PricePaid);
+        }
+
+        private string FormatPrice(MinecraftSocket socket, long number)
+        {
+            return $"{McColorCodes.AQUA}{socket.FormatPrice(number)}{McColorCodes.GRAY}";
+        }
+
     }
 }
