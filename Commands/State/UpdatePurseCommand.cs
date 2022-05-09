53f2aac9db54d0db9035e8d82bcfb47feb290c76
Äkwav <xekwavx@gmail.com>
2022-05-09 19:40:40 +0200

    hook up updatepurse to max cost

--- Commands/State/UpdatePurseCommand.cs
+++ Commands/State/UpdatePurseCommand.cs
@@ -4,10 +4,10 @@ namespace Coflnet.Sky.Commands.MC
 {
     public class UpdatePurseCommand : McCommand
     {
-        public override Task Execute(MinecraftSocket socket, string arguments)
+        public override async Task Execute(MinecraftSocket socket, string arguments)
         {
             socket.Settings.MaxCost = int.Parse(arguments.Trim('"'));
-            return Task.CompletedTask;
+            await socket.sessionLifesycle.FlipSettings.Update(socket.Settings);
         }
     }
 }
