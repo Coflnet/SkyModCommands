ed304ac288849627b69cb67cd9041981cfc31df4
Äkwav <xekwavx@gmail.com>
2022-05-23 16:44:00 +0200

    fix typos

--- Commands/ModSessionLifesycle.cs
+++ Commands/ModSessionLifesycle.cs
@@ -535,7 +535,7 @@ namespace Coflnet.Sky.Commands.MC
                         SessionInfo.Penalty += TimeSpan.FromMilliseconds(200);
                     if (penalty.Item2 > 3 && AccountInfo.Value.LastCaptchaSolve < DateTime.Now - TimeSpan.FromHours(1))
                     {
-                        SendMessage("Hello there, you acted suspiciusly like a macro bot, please select the correct answer to proof that you are not, you are delayed until you do");
+                        SendMessage("Hello there, you acted suspiciously like a macro bot (flipped consistently for multiple hours). \nplease select the correct answer to prove that you are not.", null, "You are delayed until you do");
                         SendMessage(new CaptchaGenerator().SetupChallenge(SessionInfo));
                         SessionInfo.Penalty += TimeSpan.FromSeconds(12);
                     }
