97c07be8cec3f97b6cb10589ebb6af526fe31c9b
Äkwav <ekwav@coflnet.com>
2022-07-10 14:02:10 +0200

    add missing semicoleon -_-

--- Commands/State/ChatBatchCommand.cs
+++ Commands/State/ChatBatchCommand.cs
@@ -12,7 +12,7 @@ namespace Coflnet.Sky.Commands.MC
         {
             var batch = JsonConvert.DeserializeObject<string[]>(arguments);
             if (batch[0] == "You cannot view this auction!")
-                socket.SendMessage(COFLNET + "You have to use a booster cookie or be on the hub island to open auctions. \nClick to warp to hub", "/hub", "warp to hup")
+                socket.SendMessage(COFLNET + "You have to use a booster cookie or be on the hub island to open auctions. \nClick to warp to hub", "/hub", "warp to hup");
             //socket.SendCommand("debug", "messages received " + JsonConvert.SerializeObject(batch));
             // does nothing for now
             return Task.CompletedTask;
