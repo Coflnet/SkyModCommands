8f7ef0bb22bb89bd5b4dfe24a2e0acff7b948fcf
Äkwav <ekwav@coflnet.com>
2022-08-21 19:36:39 +0200

    return error for failed derpy command

--- Commands/Minecraft/DerpyCommand.cs
+++ Commands/Minecraft/DerpyCommand.cs
@@ -8,11 +8,21 @@ namespace Coflnet.Sky.Commands.MC
     {
         public override async Task Execute(MinecraftSocket socket, string arguments)
         {
-            if(arguments.Trim('"') == "time")
+            if (arguments.Trim('"') == "time")
             {
                 var userApi = socket.GetService<UserApi>();
-                await userApi.UserUserIdServicePurchaseProductSlugPostAsync(socket.sessionLifesycle.UserId, "premium-derpy", "modCommand" + DateTime.Now.Date.ToShortDateString());
-                socket.SendMessage(COFLNET + $"You purchased 5 days of premium with your derpy compensation");
+                try
+                {
+                    await userApi.UserUserIdServicePurchaseProductSlugPostAsync(socket.sessionLifesycle.UserId, "premium-derpy", "modCommand" + DateTime.Now.Date.ToShortDateString());
+                    socket.SendMessage(COFLNET + $"You purchased 5 days of premium with your derpy compensation");
+                }
+                catch (Payments.Client.Client.ApiException e)
+                {
+                    if (e.Message.Contains("same reference"))
+                        socket.SendMessage(COFLNET + $"You already bought 5 days of premium today, the second execution is blocked to prevent you from accidentally spending more than 400 coins");
+                    else
+                        socket.SendMessage(COFLNET + $"Could not buy 5 day premium because payment system returned an error: {McColorCodes.RED}{e.Message.Substring(57).TrimEnd('}', '"')}");
+                }
                 return;
             }
             socket.SendMessage(COFLNET + $"Hello there, this command allows you to purchase premium for 5 days with your derpy compensation.\nTo purchase premium, type {McColorCodes.AQUA}/cofl derpy time");
