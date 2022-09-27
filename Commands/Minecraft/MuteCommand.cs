a5cceb706328396c98c17b24bb61ad75b74c8854
Äkwav <ekwav@coflnet.com>
2022-09-27 18:51:46 +0200

    remove unecessary usings

--- Commands/Minecraft/MuteCommand.cs
+++ Commands/Minecraft/MuteCommand.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
-using Coflnet.Sky.Core;
 using Coflnet.Sky.ModCommands.Dialogs;
 
 namespace Coflnet.Sky.Commands.MC
