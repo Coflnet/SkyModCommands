ab7b3e97cc1ae7d676c3f58c4dbde5478d42267b
Äkwav <ekwav@coflnet.com>
2022-12-21 13:42:54 +0100

    fix #44

--- Commands/Fairness/DelayCommand.cs
+++ Commands/Fairness/DelayCommand.cs
@@ -27,35 +27,47 @@ namespace Coflnet.Sky.Commands.MC
                         null, McColorCodes.GRAY + "Your call to this has been recorded, \nattempts to trick the system will be punished.");
             if (delayAmount >= TimeSpan.FromSeconds(1))
             {
-                socket.SendMessage(DialogBuilder.New
-                    .MsgLine("One of these is probably the reason you have such a high delay:")
-                    .MsgLine(FormatTimeWithReason(1, "Anti macro delay to balance flips"), null,
-                            FormatLines(
-                                "This occurs if you have a low ping or use a macro.",
-                                "Don't worry, everyone gets delayed the same way.",
-                                "You can reduce this by buying slower."))
-                    .MsgLine(FormatTimeWithReason(2, "Default delay for new connections, removed after a few seconds"), null, "Gets removed after a few seconds, just wait a bit.")
-                    .MsgLine(FormatTimeWithReason(3, "You haven't verified your minecraft account"),
-                            "https://sky.coflnet.com/player/" + socket.SessionInfo.McUuid,
-                            FormatLines(
-                                "To verify your minecraft account, bid",
-                                "the exact amount the message says on a random auction.",
-                                "Alternatively click this link and then `Claim account`."))
-                    .MsgLine(FormatTimeWithReason(8, "One of your accounts got blacklisted for bad behaviour"), null,
-                            FormatLines(
-                                "Your account was manually reviewed",
-                                "and found to be actively violating the rules.",
-                                "Most likely you used unfair advantages over extended periods of time.",
-                                "You can't remove this delay after it has been activated"))
-                    .CoflCommand<CaptchaCommand>(FormatTimeWithReason(12, "You haven't solved the anti afk captcha, click to get one"),
-                            "",
-                            FormatLines(
-                                "Look above for the message requesting you to solve the captcha.",
-                                $"If you can't find it do {McColorCodes.AQUA}/cofl captcha{McColorCodes.YELLOW} to get a new one.",
-                                "The delay will be removed after you solve the captcha 10 seconds before the next api update"))
+                socket.SendMessage(GetSupportText(socket, delayAmount)
                      );
             }
         }
+
+        private static DialogBuilder GetSupportText(MinecraftSocket socket, TimeSpan delayAmount)
+        => DialogBuilder.New
+            .MsgLine("The cause for your delay could be (hover for info):")
+            .If(() => delayAmount < TimeSpan.FromSeconds(2), db => 
+                db.MsgLine(FormatTimeWithReason(1, "Fairness delay to balance flips"), null,
+                    FormatLines(
+                        "This occurs if you have a low ping or use a macro.",
+                        "Don't worry, everyone gets delayed the same way.",
+                        "You can reduce this by buying slower.")))
+            .If(() => delayAmount == TimeSpan.FromSeconds(2), db => db
+                .MsgLine(FormatTimeWithReason(2, "Default delay for new connections, removed after a few seconds"), null, 
+                    FormatLines(
+                        "Gets removed after a few seconds, just wait a bit.",
+                        "This delay exists to prevent spamming the server with new connections")))
+            .If(() => delayAmount == TimeSpan.FromSeconds(3), db => db
+                .MsgLine(FormatTimeWithReason(3, "You haven't verified your minecraft account"),
+                    "https://sky.coflnet.com/player/" + socket.SessionInfo.McUuid,
+                    FormatLines(
+                        "To verify your minecraft account, bid",
+                        "the exact amount the message says on a random auction.",
+                        "Alternatively click this link and then `Claim account`.")))
+            .If(() => delayAmount >= TimeSpan.FromSeconds(8), db => db
+                .MsgLine(FormatTimeWithReason(8, "One of your accounts got blacklisted for bad behaviour"), null,
+                    FormatLines(
+                        "Your account was manually reviewed",
+                        "and found to be actively violating the rules.",
+                        "Most likely you used unfair advantages over extended periods of time.",
+                        "You can't remove this delay after it has been activated")))
+            .If(() => delayAmount == TimeSpan.FromSeconds(12), db => db
+                .CoflCommand<CaptchaCommand>(FormatTimeWithReason(12, $"You haven't solved the anti afk captcha, {McColorCodes.ITALIC}click to get one"),
+                    "",
+                    FormatLines(
+                        "You flipped for 4 or more hours in the last day.",
+                        $"Do {McColorCodes.AQUA}/cofl captcha{McColorCodes.YELLOW} to get a new captcha (or click this).",
+                        "The delay will be removed after you solve the captcha 10 seconds before the next api update")));
+
 
         private static string FormatLines(params string[] lines)
         {
