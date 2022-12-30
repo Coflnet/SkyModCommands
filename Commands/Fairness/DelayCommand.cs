8397d4a6a1a1d0a0672f360abe2601f9cb7ab83b
Äkwav <ekwav@coflnet.com>
2022-12-30 14:14:52 +0100

    update delay response when captcha required

--- Commands/Fairness/DelayCommand.cs
+++ Commands/Fairness/DelayCommand.cs
@@ -20,8 +20,8 @@ namespace Coflnet.Sky.Commands.MC
 
             if (delayAmount <= System.TimeSpan.Zero)
                 socket.SendMessage(COFLNET + $"You are currently not delayed at all :)", null, "Enjoy flipping at full speed☻");
-            else if (delayAmount == TimeSpan.FromSeconds(0.312345))
-                socket.SendMessage(COFLNET + $"You are minimally delayed to compensate for your macro :)", null, ";)");
+            else if (delayAmount == TimeSpan.FromSeconds(12))
+                socket.Dialog(db=>db.CoflCommand<CaptchaCommand>($"You flipped for too long and have to solve a captcha to remove your 12 second delay {McColorCodes.AQUA}click to get one", "", "Generates a new captcha"));
             else
                 socket.SendMessage(COFLNET + $"You are currently delayed by a maximum of {McColorCodes.AQUA}{delayAmount.TotalSeconds}s{McColorCodes.GRAY} by the fairness system. This will decrease over time and is not fully applied to all flips.",
                         null, McColorCodes.GRAY + "Your call to this has been recorded, \nattempts to trick the system will be punished.");
