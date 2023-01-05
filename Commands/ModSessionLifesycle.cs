47bed464d972f1f9a9b2f6d20af9264d133aa269
Äkwav <ekwav@coflnet.com>
2023-01-05 11:57:21 +0100

    userid can now be null

--- Commands/ModSessionLifesycle.cs
+++ Commands/ModSessionLifesycle.cs
@@ -214,7 +214,7 @@ namespace Coflnet.Sky.Commands.MC
         {
             using var span = socket.CreateActivity("AuthUpdate", ConSpan)
                     .AddTag("premium", info.Tier.ToString())
-                    .AddTag("userId", info.UserId.ToString());
+                    .AddTag("userId", info.UserId?.ToString());
 
             try
             {
