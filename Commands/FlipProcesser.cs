55c4eed2f6358594d0c55448ecfb1b7805bd6de3
Äkwav <ekwav@coflnet.com>
2023-01-18 16:29:19 +0100

    only show rr for actual rr

--- Commands/FlipProcesser.cs
+++ Commands/FlipProcesser.cs
@@ -235,7 +235,8 @@ namespace Coflnet.Sky.Commands.MC
                 return;
             }
             await socket.ModAdapter.SendFlip(item).ConfigureAwait(false);
-            await socket.TriggerTutorial<ModCommands.Tutorials.RoundRobinTutorial>().ConfigureAwait(false);
+            if (flip.Auction.Context.ContainsKey("isRR"))
+                await socket.TriggerTutorial<ModCommands.Tutorials.RoundRobinTutorial>().ConfigureAwait(false);
 
             _ = socket.TryAsyncTimes(async () =>
             {
