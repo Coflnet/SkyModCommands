6cdb58e1394d2006d79c771d899dfd7f5c7407a4
Äkwav <ekwav@coflnet.com>
2023-02-11 13:26:07 +0100

    improve formatting and add option to get profit summary

--- Commands/Minecraft/PreApiCommand.cs
+++ Commands/Minecraft/PreApiCommand.cs
@@ -12,22 +12,22 @@ public class PreApiCommand : McCommand
         var preapiService = socket.GetService<PreApiService>();
         if (args[0] == "notify")
         {
-            preapiService.AddNotify(preapiService.PreApiUserCount - 1, socket);
+            var notifyAtCount = preapiService.PreApiUserCount - 1;
+            if (args.Length > 1)
+            {
+                if (!int.TryParse(args[1], out notifyAtCount))
+                {
+                    socket.SendMessage($"{COFLNET}{McColorCodes.RED}Invalid number for /cofl preapi notify ");
+                    return;
+                }
+            }
+            preapiService.AddNotify(notifyAtCount, socket);
             socket.SendMessage($"{COFLNET}{McColorCodes.GREEN}You will be notified when there are less than {preapiService.PreApiUserCount} users using pre-api");
             return;
         }
         if (args[0] == "profit")
         {
-            socket.SendMessage($"Loading up all flips of the last day and calculating the profit...", null, "Can take a few seconds");
-            var profit = await socket.GetService<FlipTrackingService>().GetPreApiProfit();
-            var white = McColorCodes.WHITE;
-            socket.Dialog(db => db.MsgLine($"A total of {McColorCodes.AQUA}{profit.PlayerCount}{white} users earned at least {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit)} coins{white}. \n"
-                + $"Profit per hour being {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.HourCount))} {McColorCodes.GRAY}(only including already sold items)", null,
-                $"Profit per flip: {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.FlipCount))}\n"
-                + $"Profit per player: {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.PlayerCount))}\n"
-                + $"Profit per hour: {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.HourCount))}\n"
-                + $"Best flip: {profit.BestProfitName} {McColorCodes.GREEN}+{socket.FormatPrice(profit.BestProfit)}\n"
-                ));
+            await SendBackLastDaysProfit(socket);
             return;
         }
         if (await socket.UserAccountTier() >= Shared.AccountTier.SUPER_PREMIUM)
@@ -39,6 +39,21 @@ public class PreApiCommand : McCommand
             $"{McColorCodes.GOLD}You currently don't have {McColorCodes.RED}pre-api\n"
             + $"{McColorCodes.YELLOW}You can click this to purchase it\n",
             "pre_api", $"Click to purchase pre-api")
-            .CoflCommand<PreApiCommand>($"notify me when there are less than {preapiService.PreApiUserCount} users using it", "notify", "Click to get notified"));
+            .CoflCommand<PreApiCommand>($"{McColorCodes.GREEN}[{McColorCodes.WHITE}notify me when there are less than {preapiService.PreApiUserCount} users using it{McColorCodes.GREEN}]", "notify", "Click to get notified").Break
+            .CoflCommand<PreApiCommand>($"{McColorCodes.GREEN}[{McColorCodes.GRAY}Get last days profit{McColorCodes.GREEN}]", "profit", "Click to see the profit"));
+    }
+
+    private static async Task SendBackLastDaysProfit(MinecraftSocket socket)
+    {
+        socket.SendMessage($"Loading up all flips of the last day and calculating the profit...", null, "Can take a few seconds");
+        var profit = await socket.GetService<FlipTrackingService>().GetPreApiProfit();
+        var white = McColorCodes.WHITE;
+        socket.Dialog(db => db.MsgLine($"A total of {McColorCodes.AQUA}{profit.PlayerCount}{white} users earned at least {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit)} coins{white}. \n"
+            + $"Profit per hour being {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.HourCount))} {McColorCodes.GRAY}(only including already sold items)", null,
+            $"Profit per flip: {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.FlipCount))}\n"
+            + $"Profit per player: {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.PlayerCount))}\n"
+            + $"Profit per hour: {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.HourCount))}\n"
+            + $"Best flip: {profit.BestProfitName} {McColorCodes.GREEN}+{socket.FormatPrice(profit.BestProfit)}\n"
+            ));
     }
 }
