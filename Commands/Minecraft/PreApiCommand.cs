b74c02a39ef39a9d82e261701941ebba4e925021
Äkwav <ekwav@coflnet.com>
2023-02-18 12:01:44 +0100

    switch to estimated profit for preapi

--- Commands/Minecraft/PreApiCommand.cs
+++ Commands/Minecraft/PreApiCommand.cs
@@ -50,7 +50,8 @@ public class PreApiCommand : McCommand
         var profit = await socket.GetService<FlipTrackingService>().GetPreApiProfit();
         var white = McColorCodes.WHITE;
         socket.Dialog(db => db.MsgLine($"A total of {McColorCodes.AQUA}{profit.PlayerCount}{white} users earned at least {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit)} coins{white}. \n"
-            + $"Average profit per hour was {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.HourCount))} {McColorCodes.GRAY}(only already sold items)\n"
+            + $"All flips bought had an estimated profit of {McColorCodes.AQUA}{socket.FormatPrice(profit.ProfitSent)}\n"
+            + $"Average profit per hour was {McColorCodes.AQUA}{socket.FormatPrice(profit.ProfitSent / Math.Max(1, profit.HourCount))} {McColorCodes.GRAY}(based on sniper or median)\n"
             + $"The user with the most profit made {McColorCodes.AQUA}{socket.FormatPrice(profit.MostUserProfit)}", null,
             $"Profit per flip: {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.FlipCount))}\n"
             + $"Profit per player: {McColorCodes.AQUA}{socket.FormatPrice(profit.Profit / Math.Max(1, profit.PlayerCount))}\n"
