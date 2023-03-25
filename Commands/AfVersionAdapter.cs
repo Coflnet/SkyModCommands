a11e5e12d1db329c2382903b1dfe3ebb21f54a55
Äkwav <ekwav@coflnet.com>
2023-03-25 12:00:07 +0100

    only use recent

--- Commands/AfVersionAdapter.cs
+++ Commands/AfVersionAdapter.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Coflnet.Sky.Api.Client.Api;
 using Coflnet.Sky.Commands.Shared;
 using Coflnet.Sky.Core;
+using Newtonsoft.Json;
 
 namespace Coflnet.Sky.Commands.MC
 {
@@ -34,6 +35,7 @@ namespace Coflnet.Sky.Commands.MC
         {
             if (DateTime.UtcNow - lastListing < TimeSpan.FromSeconds(15))
                 return;
+            using var span = socket.CreateActivity("listAuction", socket.ConSpan);
             lastListing = DateTime.UtcNow;
             var apiService = socket.GetService<IPlayerApi>();
             var filters = new Dictionary<string, string>() { { "EndAfter", DateTime.UtcNow.ToUnix().ToString() } };
@@ -50,6 +52,7 @@ namespace Coflnet.Sky.Commands.MC
             var sniperService = socket.GetService<ISniperClient>();
             var values = await sniperService.GetPrices(inventory);
             var toList = inventory.Zip(values).Where(x => x.First != null && x.Second.Median > 1000);
+            span.Log(JsonConvert.SerializeObject(socket.LastSent));
             foreach (var item in toList)
             {
                 var index = inventory.IndexOf(item.First);
@@ -57,11 +60,13 @@ namespace Coflnet.Sky.Commands.MC
                 var foundInSent = socket.LastSent.Any(x => x.Auction.FlatenedNBT.FirstOrDefault(y => y.Key == "uid").Value == uid);
                 if (!foundInSent && !string.IsNullOrEmpty(uid))
                 {
-                    filters = new Dictionary<string, string>() { { "Uid", uid } };
+                    filters = new Dictionary<string, string>() { { "Uid", uid }, { "EndAfter", (DateTime.UtcNow - TimeSpan.FromHours(1)).ToUnix().ToString() } };
                     var purchases = await apiService.ApiPlayerPlayerUuidBidsGetAsync(socket.SessionInfo.McUuid, 0, filters);
-                    if(purchases.Count == 0)
+                    span.Log($"Found {purchases.Count} purchases of {item.First.ItemName}");
+                    if (purchases.Count == 0)
                         continue; // not bought, keep existing items
                 }
+                span.Log($"Listing {item.First.ItemName} for {item.Second.Median * 0.95} (median: {item.Second.Median})");
                 socket.Send(Response.Create("createAuction", new
                 {
                     Slot = index,
