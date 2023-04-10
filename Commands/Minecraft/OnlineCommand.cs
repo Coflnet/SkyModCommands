de8fbdfe5ae17500f86002b9c33d4ea7d938d3ae
Äkwav <ekwav@coflnet.com>
2023-04-10 10:44:21 +0200

    add info when hour expires to /cofl online

--- Commands/Minecraft/OnlineCommand.cs
+++ Commands/Minecraft/OnlineCommand.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
 using Coflnet.Payments.Client.Api;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System;
 
 namespace Coflnet.Sky.Commands.MC
 {
@@ -13,12 +15,13 @@ namespace Coflnet.Sky.Commands.MC
             socket.SendMessage(COFLNET + $"There are {McColorCodes.AQUA}{FlipperService.Instance.PremiumUserCount}{McColorCodes.GRAY} users connected to this server",
                     null, McColorCodes.GRAY + "there is more than one server");
             var countTask = socket.GetService<Commands.FlipTrackingService>().ActiveFlipperCount();
-            var preApiUsers = await socket.GetService<ProductsApi>().ProductsServiceServiceSlugCountGetAsync(socket.GetService<IConfiguration>()["PRODUCTS:PRE_API"]);
+            var preApiUsers = await socket.GetService<ProductsApi>().ProductsServiceServiceSlugOwnedGetAsync(socket.GetService<IConfiguration>()["PRODUCTS:PRE_API"], start: System.DateTime.UtcNow.AddHours(-1));
             string extra = McColorCodes.ITALIC + " click to buy";
             if(await socket.UserAccountTier() == AccountTier.SUPER_PREMIUM)
                 extra = McColorCodes.GREEN + " (you are one of them)";
-            socket.Dialog(db=>db.CoflCommand<PurchaseCommand>($"{McColorCodes.AQUA}{preApiUsers}{McColorCodes.GRAY} users are using {McColorCodes.RED}pre api{McColorCodes.GRAY}" + extra,
-                    "pre_api", $"they are counted in the total, {McColorCodes.AQUA}click to buy an hour"));
+            var drop = preApiUsers.OrderBy(u=>u.End).Select(u=>u.End).FirstOrDefault(DateTime.UtcNow) - DateTime.UtcNow;
+            socket.Dialog(db=>db.CoflCommand<PurchaseCommand>($"{McColorCodes.AQUA}{preApiUsers.Count}{McColorCodes.GRAY} users are using {McColorCodes.RED}pre api{McColorCodes.GRAY}" + extra,
+                    "pre_api", $"they are counted in the total, {McColorCodes.AQUA}click to buy an hour\n{McColorCodes.GRAY}The user count will drop in {drop.Minutes} minutes"));
             socket.SendMessage(COFLNET + $"{McColorCodes.AQUA}{await countTask}{McColorCodes.GRAY} players clicked on a flip in the last 3 minutes.",
                     null, McColorCodes.GRAY + "across all plans (free included)");
         }
