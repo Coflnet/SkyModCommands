6301ae6cba69278dbaccc3924fef11533cf68a0f
Äkwav <ekwav@coflnet.com>
2023-04-10 10:45:36 +0200

    reword ending of hour

--- Commands/Minecraft/OnlineCommand.cs
+++ Commands/Minecraft/OnlineCommand.cs
@@ -21,7 +21,7 @@ namespace Coflnet.Sky.Commands.MC
                 extra = McColorCodes.GREEN + " (you are one of them)";
             var drop = preApiUsers.OrderBy(u=>u.End).Select(u=>u.End).FirstOrDefault(DateTime.UtcNow) - DateTime.UtcNow;
             socket.Dialog(db=>db.CoflCommand<PurchaseCommand>($"{McColorCodes.AQUA}{preApiUsers.Count}{McColorCodes.GRAY} users are using {McColorCodes.RED}pre api{McColorCodes.GRAY}" + extra,
-                    "pre_api", $"they are counted in the total, {McColorCodes.AQUA}click to buy an hour\n{McColorCodes.GRAY}The user count will drop in {drop.Minutes} minutes"));
+                    "pre_api", $"they are counted in the total, {McColorCodes.AQUA}click to buy an hour\n{McColorCodes.GRAY}The next hour ends in {drop.Minutes} minutes"));
             socket.SendMessage(COFLNET + $"{McColorCodes.AQUA}{await countTask}{McColorCodes.GRAY} players clicked on a flip in the last 3 minutes.",
                     null, McColorCodes.GRAY + "across all plans (free included)");
         }
