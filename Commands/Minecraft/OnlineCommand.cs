8909684fee85df90424195e3a49f0a6d570c2bbe
Äkwav <ekwav@coflnet.com>
2023-04-17 17:05:50 +0200

    disable preapi

--- Commands/Minecraft/OnlineCommand.cs
+++ Commands/Minecraft/OnlineCommand.cs
@@ -20,8 +20,9 @@ namespace Coflnet.Sky.Commands.MC
             if(await socket.UserAccountTier() == AccountTier.SUPER_PREMIUM)
                 extra = McColorCodes.GREEN + " (you are one of them)";
             var drop = preApiUsers.OrderBy(u=>u.End).Select(u=>u.End).FirstOrDefault(DateTime.UtcNow) - DateTime.UtcNow;
-            socket.Dialog(db=>db.CoflCommand<PurchaseCommand>($"{McColorCodes.AQUA}{preApiUsers.Count}{McColorCodes.GRAY} users are using {McColorCodes.RED}pre api{McColorCodes.GRAY}" + extra,
-                    "pre_api", $"they are counted in the total, {McColorCodes.AQUA}click to buy an hour\n{McColorCodes.GRAY}The next hour ends in {drop.Minutes} minutes"));
+            //socket.Dialog(db=>db.CoflCommand<PurchaseCommand>($"{McColorCodes.AQUA}{preApiUsers.Count}{McColorCodes.GRAY} users are using {McColorCodes.RED}pre api{McColorCodes.GRAY}" + extra,
+            //        "pre_api", $"they are counted in the total, {McColorCodes.AQUA}click to buy an hour\n{McColorCodes.GRAY}The next hour ends in {drop.Minutes} minutes"));
+            socket.Dialog(db => db.MsgLine($"{McColorCodes.RED}pre api{McColorCodes.GRAY} is currently disabled"));
             socket.SendMessage(COFLNET + $"{McColorCodes.AQUA}{await countTask}{McColorCodes.GRAY} players clicked on a flip in the last 3 minutes.",
                     null, McColorCodes.GRAY + "across all plans (free included)");
         }
