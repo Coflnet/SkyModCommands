9020380fb325f1303992903a2338e029690911cb
Äkwav <ekwav@coflnet.com>
2023-05-06 16:37:25 +0200

    add target price to flip command

--- Commands/BinGuiVersionAdapter.cs
+++ Commands/BinGuiVersionAdapter.cs
@@ -22,6 +22,7 @@ namespace Coflnet.Sky.Commands.MC
                 worth = worth,
                 sound = new { name = (string)(string)(socket.Settings?.ModSettings?.PlaySoundOnFlip ?? false && flip.Profit > 1_000_000 ? "note.pling" : null), pitch = 1 },
                 auction = flip.Auction,
+                target = flip.MedianPrice,
                 render = Random.Shared.Next(3) switch
                 {
                     1 => "21d837ca222cbc0bc12426f5da018c3a931b406008800960a9df112a596e7d62",
