8f21fb44703920a81ab7204c2ddf5d1aee66810b
Äkwav <ekwav@coflnet.com>
2023-06-23 23:27:18 +0200

    support leaderboard pagination and own rank

--- Commands/Flipping/LeaderboardCommand.cs
+++ Commands/Flipping/LeaderboardCommand.cs
@@ -7,7 +7,7 @@ using Coflnet.Sky.PlayerName.Client.Api;
 
 namespace Coflnet.Sky.Commands.MC;
 
-[CommandDescription("Flippers with the most profit", "Most profit in the current week")]
+[CommandDescription("Flippers with the most profit", "Most profit in the current week", "Supports pagination with /cl lb <page>")]
 public class LeaderboardCommand : McCommand
 {
     public override bool IsPublic => true;
@@ -19,12 +19,15 @@ public class LeaderboardCommand : McCommand
         var api = socket.GetService<IScoresApi>();
         var nameApi = socket.GetService<PlayerNameApi>();
         var boardSlug = $"sky-flippers-{DateTime.UtcNow.RoundDown(TimeSpan.FromDays(7)).ToString("yyyy-MM-dd")}";
-        var leaderboardData = await api.ScoresLeaderboardSlugGetAsync(boardSlug);
+        int.TryParse(arguments.Trim('"'), out var page);
+        var ownTask = api.ScoresLeaderboardSlugUserUserIdRankGetAsync(boardSlug, socket.SessionInfo.McUuid);
+        var leaderboardData = await api.ScoresLeaderboardSlugGetAsync(boardSlug, page * 10, 10);
         var names = await nameApi.PlayerNameNamesBatchPostAsync(leaderboardData.Select(d => d.UserId).ToList());
+        var rank = await ownTask;
         socket.Dialog(db => db.MsgLine($"Top players for this week:").ForEach(leaderboardData, (db, data) =>
         {
             var displayName = names.Where(n => n.Key == data.UserId).Select(d => d.Value).FirstOrDefault() ?? "unknown";
             db.MsgLine($"§6{socket.FormatPrice(data.Score)} §7{(displayName)}", $"https://sky.coflnet.com/player/{data.UserId}/flips", "See flips");
-        }));
+        }).MsgLine($"You are rank: §6{socket.FormatPrice(rank)}"));
     }
 }
