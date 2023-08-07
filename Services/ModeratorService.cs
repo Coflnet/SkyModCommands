11b8663939a41f76e575dd15dfde72247c07e654
Äkwav <16632490+Ekwav@users.noreply.github.com>
2023-08-07 11:19:27 +0200

    update me fuzz uuid

--- Services/ModeratorService.cs
+++ Services/ModeratorService.cs
@@ -6,7 +6,7 @@ namespace Coflnet.Sky.ModCommands.Services;
 public class ModeratorService
 {
     List<string> MinecraftUuids = new() {
-        "aa2a2a01f3c545829358f7db0d799c08", // mrfuzz
+        "f861d737775f4dda9db485a97a89c309", // mrfuzz
         "99ab148c61b146ed9b5d07df7b46984c", // coyu
         "cdb572dfafed43c789ae5c4c009b7019", // matis
         "384a029294fc445e863f2c42fe9709cb", // ekwav
