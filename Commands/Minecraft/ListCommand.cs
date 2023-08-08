b79446884bbb3fb0266737e4a5d61193f6f34ada
Äkwav <ekwav@coflnet.com>
2023-08-08 19:39:50 +0200

    support typed remove from list

--- Commands/Minecraft/ListCommand.cs
+++ Commands/Minecraft/ListCommand.cs
@@ -164,6 +164,8 @@ namespace Coflnet.Sky.Commands.MC
                 return;
             }
             var toRemove = await Find(socket, arguments);
+            if(toRemove.Count == 0) // if no exact match is find try searching
+                toRemove = await Search(socket, arguments);
             if (toRemove.Count == 0)
             {
                 socket.SendMessage(new DialogBuilder().MsgLine($"Could not find {McColorCodes.AQUA}{arguments}{DEFAULT_COLOR} in list"));
@@ -231,6 +233,12 @@ namespace Coflnet.Sky.Commands.MC
         {
             var list = await GetList(socket);
             return list.Where(l => GetId(l) == val).ToList();
+        }
+
+        protected virtual async Task<ICollection<TElem>> Search(MinecraftSocket socket, string val)
+        {
+            var list = await GetList(socket);
+            return list.Where(l => GetId(l).ToLower().Contains(val.ToLower())).ToList();
         }
 
         /// <summary>
