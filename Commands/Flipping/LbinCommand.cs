2e68e2836d4e35ec70f856ae02328a40bccc24d9
Äkwav <ekwav@coflnet.com>
2023-10-20 23:19:29 +0200

    log item id

--- Commands/Flipping/LbinCommand.cs
+++ Commands/Flipping/LbinCommand.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
@@ -14,15 +15,16 @@ public class LbinCommand : McCommand
     public override async Task Execute(MinecraftSocket socket, string arguments)
     {
         var filters = new Dictionary<string, string>();
-        var itemName = await parser.ParseFiltersAsync(socket, arguments, filters, FlipFilter.AllFilters);
+        var itemName = await parser.ParseFiltersAsync(socket, arguments.Trim('"'), filters, FlipFilter.AllFilters);
         var itemId = await socket.GetService<Items.Client.Api.IItemsApi>().ItemsSearchTermIdGetAsync(itemName);
+        Activity.Current.Log($"Item id: {itemId} for {itemName}");
         var fe = socket.GetService<FilterEngine>();
         using var context = new HypixelContext();
         var auctions = await fe.AddFilters(context.Auctions
                     .Where(a => a.ItemId == itemId && a.End > DateTime.Now && a.HighestBidAmount == 0 && a.Bin), filters)
                     .OrderBy(a => a.StartingBid).Take(5).ToListAsync();
         socket.Dialog(db =>
-            db.ForEach(auctions.OrderByDescending(a => a.StartingBid), (d, a) => d.MsgLine($"§e{a.StartingBid}§7: {a.End}")).Break
+            db.ForEach(auctions.OrderByDescending(a => a.StartingBid), (d, a) => d.MsgLine($"§e{a.StartingBid}§7: {a.End}", $"/viewauction {a.Uuid}", a.ItemName))
             .MsgLine($"{McColorCodes.GREEN}Trying to open the lowest bin ..."));
         var lowest = auctions.OrderBy(a => a.StartingBid).FirstOrDefault();
         if (lowest == null)
