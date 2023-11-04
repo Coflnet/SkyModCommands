595de33b3a8e412cbffd8844a58b0e26b08db42f
Äkwav <ekwav@coflnet.com>
2023-11-04 23:36:06 +0100

    retry open sellers ah

--- Commands/Flipping/AhOpenCommand.cs
+++ Commands/Flipping/AhOpenCommand.cs
@@ -6,9 +6,12 @@ public class AhOpenCommand : McCommand
 {
     public override async Task Execute(MinecraftSocket socket, string arguments)
     {
-        var name = await GetMcNameForCommand.GetName(socket, arguments);
-        using var span = socket.CreateActivity("ahopen", socket.ConSpan);
-        span.SetTag("name", name);
-        socket.ExecuteCommand($"/ah {name}");
+        await socket.TryAsyncTimes(async () =>
+        {
+            var name = await GetMcNameForCommand.GetName(socket, arguments);
+            using var span = socket.CreateActivity("ahopen", socket.ConSpan);
+            span.SetTag("name", name);
+            socket.ExecuteCommand($"/ah {name}");
+        }, "opening player ah");
     }
 }
