8ac04527357593133d0616a907bc8c669360f16f
Äkwav <ekwav@coflnet.com>
2023-12-09 14:47:44 +0100

    use FilterEngine from DI

--- Commands/Utility/FiltersCommand.cs
+++ Commands/Utility/FiltersCommand.cs
@@ -29,7 +29,7 @@ namespace Coflnet.Sky.Commands.MC
         protected override async Task<IEnumerable<FilterOptions>> GetElements(MinecraftSocket socket, string val)
         {
             var itemsApi = socket.GetService<IItemsApi>();
-            var fe = new FilterEngine();
+            var fe = socket.GetService<FilterEngine>();
             var optionsTask = itemsApi.ItemItemTagModifiersAllGetAsync("*");
             socket.SendMessage("Loading all filters with all options, this may take a while");
             var all = await optionsTask;
