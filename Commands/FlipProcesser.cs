7de50421e5d899f94664033033867e20d4c96d1b
Äkwav <ekwav@coflnet.com>
2024-01-14 14:31:15 +0100

    don't check outside of eu

--- Commands/FlipProcesser.cs
+++ Commands/FlipProcesser.cs
@@ -285,7 +285,7 @@ namespace Coflnet.Sky.Commands.MC
                     ReportCommand.TryAddingAllSettings(socket, slowSpan);
                 }
 
-                if (flip.TargetPrice - flip.Auction.StartingBid > 6_000_000)
+                if (flip.TargetPrice - flip.Auction.StartingBid > 6_000_000 && socket.CurrentRegion == Region.EU)
                     socket.GetService<PreApiService>().CheckHighProfitpurchaser(socket, flip);
             }, "tracking flip");
         }
---
+++ Commands/Region.cs
@@ -0,0 +1,6 @@
+namespace Coflnet.Sky.Commands.MC;
+internal class Region
+{
+    public static string EU => "eu";
+}
+
