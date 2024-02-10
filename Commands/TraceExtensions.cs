6df0abd7617cf28bde3018fd2e87bb9b00ca3824
Äkwav <ekwav@coflnet.com>
2024-02-10 12:54:49 +0100

    reduce log context size

--- Commands/TraceExtensions.cs
+++ Commands/TraceExtensions.cs
@@ -9,7 +9,7 @@ public static class TraceExtensions
 {
     public static Activity? Log(this Activity? activity, string message)
     {
-        return activity?.AddEvent(new ActivityEvent("log", System.DateTimeOffset.Now, new ActivityTagsCollection(new[] { new KeyValuePair<string, object?>("message", message.Truncate(38_000)) })));
+        return activity?.AddEvent(new ActivityEvent("log", System.DateTimeOffset.Now, new ActivityTagsCollection(new[] { new KeyValuePair<string, object?>("message", message.Truncate(6_000)) })));
     }
 }
 
