bfcee7b9a333578505235559452e6e226b1fbcfe
Äkwav <ekwav@coflnet.com>
2024-04-18 09:58:49 +0200

    update MrFuzzBall account

--- Services/ModeratorService.cs
+++ Services/ModeratorService.cs
@@ -6,7 +6,7 @@ namespace Coflnet.Sky.ModCommands.Services;
 public class ModeratorService
 {
     private List<string> MinecraftUuids = new() {
-        "1adeea4aaf0f43179b22fc6fb63ad2c6", // mrfuzz
+        "aa2a2a01f3c545829358f7db0d799c08", // mrfuzz
         "99ab148c61b146ed9b5d07df7b46984c", // coyu
         "cdb572dfafed43c789ae5c4c009b7019", // matis
         "384a029294fc445e863f2c42fe9709cb", // ekwav
