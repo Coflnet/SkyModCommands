25ec421b100d43f1c40feb287e71a547204a9412
Äkwav <ekwav@coflnet.com>
2024-05-08 21:20:48 +0200

    make tab rift check more percise

--- Commands/State/UploadTabCommand.cs
+++ Commands/State/UploadTabCommand.cs
@@ -6,7 +6,7 @@ namespace Coflnet.Sky.Commands.MC
     {
         public override Task Execute(MinecraftSocket socket, string arguments)
         {
-            if (arguments.Contains("Rift"))
+            if (arguments.Contains("The Rift"))
                 MinecraftSocket.Commands["uploadscoreboard"].Execute(socket, arguments);
             // does nothing for now
             return Task.CompletedTask;
