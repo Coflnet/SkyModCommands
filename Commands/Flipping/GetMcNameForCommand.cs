e007d5e22f5e5f6993f11cffc8dc399151cd0fec
Äkwav <ekwav@coflnet.com>
2024-06-01 18:53:24 +0200

    correct get name

--- Commands/Flipping/GetMcNameForCommand.cs
+++ Commands/Flipping/GetMcNameForCommand.cs
@@ -7,7 +7,7 @@ public class GetMcNameForCommand : McCommand
     public override async Task Execute(MinecraftSocket socket, string arguments)
     {
         string name = await GetName(socket, arguments);
-        socket.Dialog(db => db.Msg("This auction was sold by " + McColorCodes.AQUA + name));
+        socket.Dialog(db => db.Msg("The minecraft name is " + McColorCodes.AQUA + name));
     }
 
     public static async Task<string> GetName(MinecraftSocket socket, string arguments)
@@ -15,7 +15,7 @@ public class GetMcNameForCommand : McCommand
         var uuid = Newtonsoft.Json.JsonConvert.DeserializeObject<string>(arguments);
         var name = await socket.GetPlayerName(uuid);
         if (name == null)
-            throw new Core.CoflnetException("name_not_found", "Could not retrieve the sellers name :(");
+            throw new Core.CoflnetException("name_not_found", "Could not retrieve the account name :(");
         return name;
     }
 }
--- Dialogs/ChatOptionsDialog.cs
+++ Dialogs/ChatOptionsDialog.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Coflnet.Sky.Commands.MC;
 using Coflnet.Sky.ModCommands.Services;
 
@@ -8,18 +9,19 @@ namespace Coflnet.Sky.ModCommands.Dialogs
         public override ChatPart[] GetResponse(DialogArgs context)
         {
             var userName = context.Context.Split(' ')[0];
-            if(userName == context.socket.SessionInfo.McName)
+            if (userName == context.socket.SessionInfo.McName)
             {
                 return New()
                     .Msg("There are no options for your own messages")
                     .LineBreak();
             }
+            var uuid = context.Context.Split(' ').Last();
             var isModerator = context.socket.GetService<ModeratorService>().IsModerator(context.socket);
             return New()
                 .Msg("What do you want to do?")
                 .LineBreak()
                 .CoflCommand<MuteCommand>("  Mute " + userName, userName, McColorCodes.GRAY + "I don't want to get anymore messages from this user").LineBreak()
-                .CoflCommand<GetMcNameForCommand>("  Get IGN", userName, McColorCodes.GRAY + "Get the IGN of this user\n(player might be nicked)").LineBreak()
+                .CoflCommand<GetMcNameForCommand>("  Get IGN", uuid, $"Get the IGN of this user\n{McColorCodes.GRAY}(player might be nicked)").LineBreak()
                 .If(() => !isModerator, db => db.DialogLink<ChatReportDialog>("  Report this message ", context.Context, McColorCodes.GRAY + "report message to moderator").LineBreak())
                 .CoflCommand<ChatCommand>(" -> disable chat", "", McColorCodes.GRAY + "turn off showing this chat").LineBreak()
                 .If(() => isModerator, db => db.CoflCommand<GlobalMuteCommand>("Global chat mute the user for rule 1", "1 " + context.Context, $"x10 mute time for rule 1")).LineBreak()
