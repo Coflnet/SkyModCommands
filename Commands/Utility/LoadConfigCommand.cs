6bc9da0783541723b354fac518d8159f6a5eb1c3
Äkwav <ekwav@coflnet.com>
2024-06-17 16:17:37 +0200

    allow export of own config

--- Commands/Utility/LoadConfigCommand.cs
+++ Commands/Utility/LoadConfigCommand.cs
@@ -14,14 +14,14 @@ public class LoadConfigCommand : ArgumentsCommand
         var name = args["configName"];
         var key = SellConfigCommand.GetKeyFromname(name);
         var ownedConfigs = await SelfUpdatingValue<OwnedConfigs>.Create(socket.UserId, "owned_configs", () => new());
-        var inOwnerShip = ownedConfigs.Value.Configs.Where(c => c.Name.Equals(name, System.StringComparison.InvariantCultureIgnoreCase) && c.OwnerId == owner).FirstOrDefault() 
+        var inOwnerShip = ownedConfigs.Value.Configs.Where(c => c.Name.Equals(name, System.StringComparison.InvariantCultureIgnoreCase) && c.OwnerId == owner).FirstOrDefault()
             ?? ownedConfigs.Value.Configs.Where(c => c.Name.Equals(name, System.StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
         if (inOwnerShip == default)
         {
             socket.SendMessage("You don't own this config.");
             return;
         }
-        if(!int.TryParse(owner, out _))
+        if (!int.TryParse(owner, out _))
         {
             owner = inOwnerShip.OwnerId;
         }
@@ -31,7 +31,7 @@ public class LoadConfigCommand : ArgumentsCommand
             socket.SendMessage("The config doesn't exist.");
             return;
         }
-        toLoad.Value.Settings.BlockExport = true;
+        toLoad.Value.Settings.BlockExport = toLoad.Value.OwnerId != socket.UserId;
         await socket.sessionLifesycle.FlipSettings.Update(toLoad.Value.Settings);
         socket.Dialog(db => db.MsgLine($"§6{toLoad.Value.Name} §7v{toLoad.Value.Version} §6loaded"));
         inOwnerShip.ChangeNotes = toLoad.Value.ChangeNotes;
