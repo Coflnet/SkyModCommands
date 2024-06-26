f81442600d33436618114aefc89f997686a9d683
Äkwav <ekwav@coflnet.com>
2024-06-26 12:00:21 +0200

    note that outside skyblock

--- Commands/Minecraft/BlockedCommand.cs
+++ Commands/Minecraft/BlockedCommand.cs
@@ -96,7 +96,12 @@ namespace Coflnet.Sky.Commands.MC
                 socket.Dialog(db => db.CoflCommand<PurchaseCommand>($"Note that you don't have premium, flips will show up very late if at all. Eg. the user finder doesn't work. \n{McColorCodes.GREEN}[Click to change that]", "", "Click to select a premium plan"));
             }
 
-            if (socket.SessionInfo.Purse != 0 && socket.SessionInfo.Purse < 10_000_000)
+            if (socket.SessionInfo.Purse == -1)
+            {
+                await Task.Delay(1000);
+                socket.Dialog(db => db.MsgLine("It looks like you are not in skyblock currently. Flips are only shown in skyblock."));
+            }
+            else if (socket.SessionInfo.Purse != 0 && socket.SessionInfo.Purse < 10_000_000)
             {
                 await Task.Delay(2000);
                 socket.Dialog(db => db.MsgLine("You don't have many coins in your purse. Flips you can't afford aren't shown."));
