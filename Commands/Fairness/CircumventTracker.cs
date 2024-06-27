907cd88f07b0af935adbb158f60c63c6c5e88a9a
Äkwav <ekwav@coflnet.com>
2024-06-28 00:09:42 +0200

    less likely for old users

--- Commands/Fairness/CircumventTracker.cs
+++ Commands/Fairness/CircumventTracker.cs
@@ -40,7 +40,7 @@ public class CircumventTracker
     public async Task<string?> CreateChallenge(IMinecraftSocket socket, bool forceCreate = false)
     {
         if (socket.SessionInfo.NotPurchaseRate <= 1 || await socket.UserAccountTier() < AccountTier.PREMIUM
-                        || (socket.AccountInfo.McIds.Count > 3 || socket.AccountInfo.ExpiresAt > DateTime.UtcNow + TimeSpan.FromDays(20)) && Random.Shared.NextDouble() < 0.9) // probably legit
+                        || (socket.AccountInfo.McIds.Count > 3 || socket.AccountInfo.ExpiresAt > DateTime.UtcNow + TimeSpan.FromDays(20) || socket.UserId.Length < 6) && Random.Shared.NextDouble() < 0.9) // probably legit
         {
             Activity.Current?.Log($"Not creating challenge because probably legit {socket.SessionInfo.NotPurchaseRate}");
             return null;
