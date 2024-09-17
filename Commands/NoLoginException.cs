b8bab17af3c3b6e08cdde3c1543b01317dbbd0cd
Äkwav <ekwav@coflnet.com>
2024-09-17 19:34:29 +0200

    Add guidance for login

--- Commands/NoLoginException.cs
+++ Commands/NoLoginException.cs
@@ -4,7 +4,7 @@ namespace Coflnet.Sky.Commands.MC
 {
     public class NoLoginException : CoflnetException
     {
-        public NoLoginException() : base("no_login", "We could not determine your user account. Please make sure to login and try again.")
+        public NoLoginException() : base("no_login", $"We could not determine your user account. Please make sure to login and try again. {McColorCodes.AQUA}/cofl verify{McColorCodes.GRAY} will help you with that.")
         {
         }
     }
