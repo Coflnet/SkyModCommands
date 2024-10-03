3ea71c705983aba40b08f825ef6cab7e8583995f
Äkwav <ekwav@coflnet.com>
2024-10-03 13:30:13 +0200

    save tricks on inventory

--- Commands/State/UploadInventory.cs
+++ Commands/State/UploadInventory.cs
@@ -8,12 +8,16 @@ namespace Coflnet.Sky.Commands.MC;
 public class UploadInventory : McCommand
 {
     private InventoryParser parser = new InventoryParser();
-    public override Task Execute(MinecraftSocket socket, string arguments)
+    public override async Task Execute(MinecraftSocket socket, string arguments)
     {
         socket.SessionInfo.Inventory = null;
         socket.SessionInfo.Inventory = parser.Parse(arguments).ToList();
         Activity.Current?.Log(JsonConvert.SerializeObject(socket.SessionInfo.Inventory));
-        // does nothing for now
-        return Task.CompletedTask;
+        if (socket.ModAdapter is AfVersionAdapter)
+        {
+            return;
+        }
+        socket.AccountInfo.Tricks.TickFound("inventory");
+        await socket.sessionLifesycle.AccountInfo.Update();
     }
 }
