b5217bc0da4a2f41c0fe673fb85d693f3598d800
Äkwav <ekwav@coflnet.com>
2024-10-17 18:45:51 +0200

    paginate museum command

--- Commands/Utility/CheapMuseumCommand.cs
+++ Commands/Utility/CheapMuseumCommand.cs
@@ -1,20 +1,28 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
+using Coflnet.Sky.ModCommands.Dialogs;
 
 namespace Coflnet.Sky.Commands.MC;
 
-public class CheapMuseumCommand : McCommand
+public class CheapMuseumCommand : ReadOnlyListCommand<MuseumService.Cheapest>
 {
     public override bool IsPublic => true;
 
-    public override async Task Execute(MinecraftSocket socket, string arguments)
+    protected override void Format(MinecraftSocket socket, DialogBuilder db, MuseumService.Cheapest item)
+    {
+        db.MsgLine($" {item.ItemName} for {McColorCodes.AQUA}{item.PricePerExp} coins {McColorCodes.GRAY}per exp",
+                    "/viewauction " + item.AuctuinUuid, "Click to view the auction");
+    }
+
+    protected override async Task<IEnumerable<MuseumService.Cheapest>> GetElements(MinecraftSocket socket, string val)
     {
         var service = socket.GetService<MuseumService>();
-        var cheapMuseum = await service.GetBestMuseumPrices();
+        return await service.GetBestMuseumPrices();
+    }
 
-        socket.Dialog(db => db.MsgLine("The cheapest museum prices are:")
-            .ForEach(cheapMuseum, (db, item) =>
-                db.MsgLine($" {item.ItemName} for {McColorCodes.AQUA}{item.PricePerExp} coins {McColorCodes.GRAY}per exp",
-                    "/viewauction " + item.AuctuinUuid, "Click to view the auction")));
+    protected override string GetId(MuseumService.Cheapest elem)
+    {
+        return elem.ItemName;
     }
 }
