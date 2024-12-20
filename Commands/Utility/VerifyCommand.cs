0c8fc3a2eca2fc7654d3f27601af19945bec01d1
Äkwav <ekwav@coflnet.com>
2024-12-20 12:41:19 +0100

    note when not allowed to send CoflCoins

--- Commands/Utility/VerifyCommand.cs
+++ Commands/Utility/VerifyCommand.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Threading.Tasks;
+using Coflnet.Payments.Client.Api;
 
 namespace Coflnet.Sky.Commands.MC;
 
@@ -16,7 +18,13 @@ public class VerifyCommand : McCommand
         var isVerified = await verifcationHandler.CheckVerificationStatus(socket.AccountInfo);
         if (isVerified)
         {
+            var transactions = await socket.GetService<ITransactionApi>().TransactionUUserIdGetAsync(socket.UserId.ToString(), 0, 50);
+            var canSendCoins = transactions.Count > 10 || transactions.Any(t => t.ProductId == "verify_mc") || transactions.Any(t => t.ProductId == "premium_plus");
             socket.SendMessage($"You are verified. You can chat with {McColorCodes.AQUA}/fc <message>");
+            if (!canSendCoins)
+            {
+                socket.SendMessage("You don't qualify for the sending of CoflCoins tho. This happens when you use multiple emails with the same minecraft account which is not allowed.");
+            }
             return;
         }
         socket.Dialog(db => db.MsgLine("You are not verified yet. Please follow the instructions above to verify your account."));
