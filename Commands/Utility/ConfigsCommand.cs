cde6eec4b04ab40dabdf54ef28d1ebf2c899ef13
Äkwav <ekwav@coflnet.com>
2025-01-02 12:02:33 +0100

    devide flip count by users

--- Commands/Utility/ConfigsCommand.cs
+++ Commands/Utility/ConfigsCommand.cs
@@ -94,7 +94,7 @@ public class ConfigsCommand : ListCommand<ConfigsCommand.ConfigRating, List<Conf
         var flipPaid = flips.Flips.Sum(f => f.PricePaid);
         Console.WriteLine($"Flips: {flipCount} Profit: {flipProfit} Paid: {flipPaid} - count {uuids.Count()} - {timeSpan}");
         var avgProfitPerDay = flipProfit / uuids.Count() + 1 / timeSpan.TotalDays;
-        var flipsPerDay = flipCount / timeSpan.TotalDays;
+        var flipsPerDay = flipCount / differentUsers / timeSpan.TotalDays;
         var mostCommonItem = flips.Flips.GroupBy(f => f.ItemTag).OrderByDescending(g => g.Count()).FirstOrDefault()?.First().ItemName ?? "none";
         var mostProfit = flips.Flips.OrderByDescending(f => f.Profit).FirstOrDefault();
 
