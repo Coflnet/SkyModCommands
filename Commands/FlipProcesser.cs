c2dd91110cc798177d62a976e46a90b605318e7b
Äkwav <ekwav@coflnet.com>
2025-01-13 14:38:52 +0100

    correct fT

--- Commands/FlipProcesser.cs
+++ Commands/FlipProcesser.cs
@@ -202,7 +202,7 @@ namespace Coflnet.Sky.Commands.MC
                 foreach (var item in flips)
                 {
                     var fullTime = DateTime.UtcNow - item.f.Auction.FindTime;
-                    if (TimeSpan.TryParse(item.f.Auction.Context?.GetValueOrDefault("ft", "xy"), out var ft))
+                    if (TimeSpan.TryParse(item.f.Auction.Context?.GetValueOrDefault("fT", "xy"), out var ft))
                     {
                         var estimate = (fullTime - ft).TotalSeconds;
                         if (estimate < 5) // ignore outliers
