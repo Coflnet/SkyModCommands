0854bb8e62ba548946c97fedce834acb5c97f83f
Äkwav <ekwav@coflnet.com>
2025-01-19 19:27:22 +0100

    don't delay any recheck

--- Commands/Flipping/LowPricedExtensions.cs
+++ Commands/Flipping/LowPricedExtensions.cs
@@ -5,7 +5,7 @@ public static class LowPricedExtensions
 {
     public static bool IsPreApi(this LowPricedAuction flip)
     {
-        return (flip.Auction.Context?.TryGetValue("pre-api", out var preApi) ?? false) && preApi != "recheck";
+        return (flip.Auction.Context?.TryGetValue("pre-api", out var preApi) ?? false) && !preApi.Contains("recheck");
     }
 
     public static LowPricedAuction ToLowPriced(this FlipInstance flip)
