64c4e2beabba97e86009c062c51d79fc5c8878dc
Äkwav <ekwav@coflnet.com>
2025-01-22 13:26:25 +0100

    update license use argument check

--- Commands/State/LicensesCommand.cs
+++ Commands/State/LicensesCommand.cs
@@ -53,7 +53,7 @@ public class LicensesCommand : ListCommand<PublicLicenseWithName, List<PublicLic
 
     private static async Task SwitchAccountInUse(MinecraftSocket socket, string[] args)
     {
-        if (args.Length < 2)
+        if (args.Length < 3)
         {
             socket.Dialog(db => db.MsgLine("Please provide the license id (first character of /cofl licenses list) and the username to switch to"));
             return;
