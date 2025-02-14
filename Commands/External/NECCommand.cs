8663ef935054134eb75f60bd5c7ddca2dda44278
Äkwav <ekwav@coflnet.com>
2025-02-14 21:29:06 +0100

    allow claiming by only email

--- Commands/External/NECCommand.cs
+++ Commands/External/NECCommand.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Cassandra;
 using Cassandra.Data.Linq;
@@ -7,6 +8,7 @@ using Cassandra.Mapping;
 using Coflnet.Core.Tracing;
 using Coflnet.Payments.Client.Api;
 using Coflnet.Sky.Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
@@ -25,28 +27,47 @@ public class NECCommand : ArgumentsCommand
         }
         var necUser = await socket.GetService<NecImportService>().GetUser(socket.SessionInfo.McUuid);
         Activity.Current.Log(JsonConvert.SerializeObject(necUser));
+        var count = 2;
         if (necUser == null || necUser.Email?.ToLower() != email && necUser.Email != null)
         {
-            socket.SendMessage($"No user with email {email} and ign {socket.SessionInfo.McName} found in the NEC database. Please check your email or ask support for help.");
-            await Task.Delay(3000);
-            return;
+            using var context = new HypixelContext();
+            var userId = int.Parse(socket.UserId);
+            var user = await context.Users.FirstOrDefaultAsync(u => u.Email == email && userId == u.Id);
+            if (user == null)
+            {
+                socket.SendMessage($"No user with email {email} and ign {socket.SessionInfo.McName} found in the NEC database. Please check your email or ask support for help.");
+                await Task.Delay(3000);
+                return;
+            }
+            necUser = await socket.GetService<NecImportService>().GetUserByEmail(email);
+            if (necUser == null)
+            {
+                socket.SendMessage($"No user with email {email} and ign {socket.SessionInfo.McName} found in the NEC database. Please check your email or ask support for help.");
+                await Task.Delay(3000);
+                return;
+            }
+            else if (!string.IsNullOrEmpty(necUser.ClaimedOnAccount))
+            {
+                socket.SendMessage($"Since you didn't claim the nec welcome bonus on the correct account its halfed");
+                count = 1;
+            }
+            Activity.Current.Log(JsonConvert.SerializeObject(necUser));
         }
         if (!string.IsNullOrEmpty(necUser.ClaimedOnAccount))
         {
             socket.SendMessage($"This account has already claimed the cofl nec tier at {McColorCodes.AQUA}{necUser.ClaimedAt}\n{McColorCodes.GRAY}It can only be claimed once.");
             return;
         }
-        socket.Dialog(db => db.MsgLine("Welcome to SkyCofl!\nWe are unlocking the NEC tier for you, please give us a second"));
+        socket.Dialog(db => db.MsgLine("Welcome to SkyCofl!\nWe are unlocking the basic tier for you, please give us a second"));
         try
         {
-            var count = 2;
             if (DateTime.UtcNow > new DateTime(2025, 8, 1))
             {
                 count = 1;
             }
             else if (DateTime.UtcNow > new DateTime(2026, 2, 1))
             {
-                throw new CoflnetException("to_late", "NEC tier is no longer available, The offer expired on 2026-02-01");
+                throw new CoflnetException("to_late", "NEC migrating is no longer available, The offer expired on 2026-02-01");
             }
             var topup = socket.GetService<TopUpApi>();
             await topup.TopUpCustomPostAsync(socket.UserId, new()
@@ -102,6 +123,12 @@ public class NecImportService
     public async Task<NecUser> GetUser(string uuid)
     {
         return await necUsers.FirstOrDefault(u => u.Uuid == uuid).ExecuteAsync();
+    }
+
+    internal async Task<NecUser> GetUserByEmail(string email)
+    {
+        var all = await necUsers.Where(u => u.Email == email).AllowFiltering().ExecuteAsync();
+        return all.FirstOrDefault();
     }
 
     public class NecUser
