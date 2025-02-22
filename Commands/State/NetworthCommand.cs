6640802b70189d31695ff247b1d1ba29a42a2bc9
Äkwav <ekwav@coflnet.com>
2025-02-23 00:02:18 +0100

    Improve networth formatting

--- Commands/State/NetworthCommand.cs
+++ Commands/State/NetworthCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Coflnet.Sky.Api.Client.Api;
@@ -11,7 +12,7 @@ public class NetworthCommand : ArgumentsCommand
 {
     public override bool IsPublic => true;
 
-    protected override string Usage => "<username> [profile=active]";
+    protected override string Usage => "<username> [profile=used]";
 
     protected override async Task Execute(IMinecraftSocket socket, Arguments args)
     {
@@ -21,7 +22,17 @@ public class NetworthCommand : ArgumentsCommand
         var pricesApi = socket.GetService<IPricesApi>();
         var accountUuid = await socket.GetPlayerUuid(username, false);
         var profileData = await profileApi.GetProfiles(accountUuid);
-        var profileId = profileData.FirstOrDefault(p => p.Key.Equals(profile, System.StringComparison.InvariantCultureIgnoreCase)).Key ?? "active";
+        var profileId = profileData.FirstOrDefault(p => p.Key.Equals(profile, System.StringComparison.InvariantCultureIgnoreCase)).Key ?? "used";
+        if (profileId == "used")
+        {
+            profileId = (await profileApi.GetActiveProfileId(accountUuid)).Trim('"');
+            Console.WriteLine($"Using active profile {profileId}");
+            var mappedName = profileData.FirstOrDefault(p => p.Value.Equals(profileId));
+            if (mappedName.Key != null)
+            {
+                profile = mappedName.Key;
+            }
+        }
         var profileInfo = await profileApi.GetProfile(accountUuid, profileId);
         var virtualFull = new Api.Client.Model.Profile()
         {
@@ -29,7 +40,7 @@ public class NetworthCommand : ArgumentsCommand
         };
         var networth = await pricesApi.ApiNetworthPostAsync(virtualFull);
         var top = networth.Member.First().Value.ValuePerCategory.OrderByDescending(m => m.Value).Take(3);
-        socket.Dialog(db => db.MsgLine($"Networth of {username} in {profile} profile is {McColorCodes.GOLD}{socket.FormatPrice(networth.FullValue)}",null, "This is currently based on api fields\nincluding chest value is in the works")
-            .ForEach(top, (db, m) => db.MsgLine($"{m.Key} {socket.FormatPrice(m.Value)}")));
+        socket.Dialog(db => db.MsgLine($"Networth of {McColorCodes.AQUA}{username} {McColorCodes.RESET}on {McColorCodes.AQUA}{profile} {McColorCodes.RESET}is {McColorCodes.GOLD}{socket.FormatPrice(networth.FullValue)}", null, "This is currently based on api fields\nincluding chest value is in the works")
+            .ForEach(top, (db, m) => db.MsgLine($"{McColorCodes.DARK_GRAY} - {McColorCodes.RESET}{m.Key} {McColorCodes.GOLD}{socket.FormatPrice(m.Value)}")));
     }
 }
