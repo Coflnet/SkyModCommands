977146387979f439be578890e34d1ba67ddb17bd
Äkwav <ekwav@coflnet.com>
2025-04-14 00:03:07 +0200

    suggest earlier

--- Commands/Flipping/PingCommand.cs
+++ Commands/Flipping/PingCommand.cs
@@ -47,7 +47,7 @@ public class PingCommand : McCommand
         var average = thisSession.Average();
         var lowest = thisSession.Min();
         db?.AddTag("minping", lowest);
-        if(average > 120 && socket.CurrentRegion == "eu")
+        if(average > 80 && socket.CurrentRegion == "eu")
             socket.Dialog(db => db.CoflCommand<SwitchRegionCommand>("Your ping is quite high, you might want to consider switching to the US region.", "us", "Click to switch to us servers"));
         Console.Write($"Ping of {ping}ms from {socket.SessionInfo.McName} {socket.ClientIp} {socket.SessionInfo.McUuid} {average}");
         Console.WriteLine($" {socket.sessionLifesycle?.UserId?.Value}");
