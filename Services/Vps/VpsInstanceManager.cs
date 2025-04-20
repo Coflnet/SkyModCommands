80d9d7287d8e395df964ceaf11efb46f63a70173
Äkwav <ekwav@coflnet.com>
2025-04-20 13:51:21 +0200

    return all values

--- Services/Vps/VpsInstanceManager.cs
+++ Services/Vps/VpsInstanceManager.cs
@@ -304,7 +304,7 @@ public class VpsInstanceManager
         var start = parsed.AddHours(-24).ToUnixTimeSeconds();
         var end = timeStamp;
         var log = await QueryLokiJson(query, start, end, 5_000);
-        return string.Join("\n", log.data.result.Select(r=>r.stream.user_id + ": " + r.values[0][1]));
+        return string.Join("\n", log.data.result.SelectMany(r=>r.values.Select(v=>r.stream.user_id + ": " + v[1])));
     }
 
     internal async Task DeleteVps(Instance instance)
