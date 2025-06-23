a1c97621bc96cb41fa167e3c0ed432b7ef4c4a7a
Äkwav <ekwav@coflnet.com>
2025-06-23 22:51:48 +0200

    use file scope namespace for flip controller

--- Controllers/FlipController.cs
+++ Controllers/FlipController.cs
@@ -9,58 +9,57 @@ using static Coflnet.Sky.ModCommands.Services.BlockedService;
 using Coflnet.Sky.Commands.MC;
 using System.Linq;
 
-namespace Coflnet.Sky.ModCommands.Controllers
+namespace Coflnet.Sky.ModCommands.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FlipController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class FlipController : ControllerBase
+    private FlipperService flipperService;
+    private IBlockedService blockedService;
+    private NecImportService necImportService;
+
+    public FlipController(FlipperService flipperService, IBlockedService blockedService, NecImportService necImportService)
     {
-        private FlipperService flipperService;
-        private IBlockedService blockedService;
-        private NecImportService necImportService;
+        this.flipperService = flipperService;
+        this.blockedService = blockedService;
+        this.necImportService = necImportService;
+    }
 
-        public FlipController(FlipperService flipperService, IBlockedService blockedService, NecImportService necImportService)
-        {
-            this.flipperService = flipperService;
-            this.blockedService = blockedService;
-            this.necImportService = necImportService;
-        }
+    [HttpGet]
+    [Route("blocked")]
+    public async Task<IEnumerable<BlockedReason>> GetBlockedReasons(string userId, string auctionUuid)
+    {
+        return await blockedService.GetBlockedReasons(userId, Guid.Parse(auctionUuid));
+    }
 
-        [HttpGet]
-        [Route("blocked")]
-        public async Task<IEnumerable<BlockedReason>> GetBlockedReasons(string userId, string auctionUuid)
+    [HttpPost]
+    [Route("nec")]
+    public async Task AddNecUser([FromBody] NecImportService.NecUser[] users)
+    {
+        var count = 0;
+        foreach (var user in users.GroupBy(u => u.Uuid).Select(g => g.OrderByDescending(u => u.Key == null ? 0 : 1).First()))
         {
-            return await blockedService.GetBlockedReasons(userId, Guid.Parse(auctionUuid));
+            await necImportService.AddUser(user);
+            count++;
         }
+        Console.WriteLine($"Added {count} users");
+    }
 
-        [HttpPost]
-        [Route("nec")]
-        public async Task AddNecUser([FromBody] NecImportService.NecUser[] users)
+    /// <summary>
+    /// Indicates status of service, should be 200 (OK)
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("")]
+    public async Task TrackFlip([FromBody] LowPricedAuction[] flips)
+    {
+        foreach (var item in flips)
         {
-            var count = 0;
-            foreach (var user in users.GroupBy(u => u.Uuid).Select(g => g.OrderByDescending(u => u.Key == null ? 0 : 1).First()))
-            {
-                await necImportService.AddUser(user);
-                count++;
-            }
-            Console.WriteLine($"Added {count} users");
+            item.Auction.Uuid = Guid.NewGuid().ToString().Replace("-", "");
+            item.Auction.Context = new() { { "pre-api", "" }, { "cname", item.Auction.ItemName } };
         }
-
-        /// <summary>
-        /// Indicates status of service, should be 200 (OK)
-        /// </summary>
-        /// <returns></returns>
-        [HttpPost]
-        [Route("")]
-        public async Task TrackFlip([FromBody] LowPricedAuction[] flips)
-        {
-            foreach (var item in flips)
-            {
-                item.Auction.Uuid = Guid.NewGuid().ToString().Replace("-", "");
-                item.Auction.Context = new() { { "pre-api", "" }, { "cname", item.Auction.ItemName } };
-            }
-            //Console.WriteLine(JsonConvert.SerializeObject(flips, Formatting.Indented));
-            await flipperService.DeliverLowPricedAuctions(flips);
-        }
+        //Console.WriteLine(JsonConvert.SerializeObject(flips, Formatting.Indented));
+        await flipperService.DeliverLowPricedAuctions(flips);
     }
 }
