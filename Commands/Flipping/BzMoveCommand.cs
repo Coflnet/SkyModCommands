c1fc5a338e563e959f249fd7eea598854688e8e8
Äkwav <ekwav@coflnet.com>
2025-07-15 15:15:49 +0200

    update formatting on bzmove and fix search

--- Commands/Flipping/BzMoveCommand.cs
+++ Commands/Flipping/BzMoveCommand.cs
@@ -18,8 +18,9 @@ public class BzMoveCommand : ReadOnlyListCommand<BzMoveCommand.MovementElement>
 
     protected override void Format(MinecraftSocket socket, DialogBuilder db, MovementElement elem)
     {
-        db.Msg($" {elem.ItemName} {McColorCodes.RED}{socket.FormatPrice(elem.Movement.PreviousPrice)} {McColorCodes.GRAY}-> {McColorCodes.GREEN}{socket.FormatPrice(elem.Movement.CurrentPrice)}", "/bz " + elem.ItemName, "open in game")
-            .Button($"Website", $"https://sky.coflnet.com/item/{elem.Movement.ItemId}", "open in browser");
+        db.Msg($" {elem.ItemName} {McColorCodes.RED}{socket.FormatPrice(elem.Movement.PreviousPrice)} {McColorCodes.GRAY}-> {McColorCodes.GREEN}{socket.FormatPrice(elem.Movement.CurrentPrice)}", "/bz " + elem.ItemName, $"open {McColorCodes.AQUA}{elem.ItemName} {McColorCodes.GRAY}in game")
+            .Button($"Website", $"https://sky.coflnet.com/item/{elem.Movement.ItemId}", $"open {McColorCodes.AQUA}{elem.ItemName} {McColorCodes.GRAY}in browser")
+            .LineBreak();
     }
 
     protected override async Task<IEnumerable<MovementElement>> GetElements(MinecraftSocket socket, string val)
@@ -27,6 +28,16 @@ public class BzMoveCommand : ReadOnlyListCommand<BzMoveCommand.MovementElement>
         var items = socket.GetService<IBazaarApi>();
         var movementTask = items.GetMovementAsync(24, val.Contains("buy"));
         var itemsApi = socket.GetService<Items.Client.Api.IItemsApi>();
+        var search = val.ToLower();
+        foreach (var item in sorters.Keys)
+        {
+            if (search.StartsWith(item))
+            {
+                search = search.Substring(item.Length).Trim();
+                break;
+            }
+        }
+        search = search.Replace("buy", "").Replace("sell", "");
 
         var names = (await itemsApi.ItemNamesGetAsync()).ToDictionary(i => i.Tag, i => i.Name);
         return (await movementTask)
@@ -35,7 +46,7 @@ public class BzMoveCommand : ReadOnlyListCommand<BzMoveCommand.MovementElement>
                 Movement = m,
                 ItemName = names.TryGetValue(m.ItemId, out var name) ? name : m.ItemId
             })
-            .Where(m => m.ItemName.Contains(val, StringComparison.OrdinalIgnoreCase))
+            .Where(m => m.ItemName.Contains(search, StringComparison.OrdinalIgnoreCase))
             .OrderByDescending(m => m.Movement.CurrentPrice - m.Movement.PreviousPrice)
             .ToList();
     }
