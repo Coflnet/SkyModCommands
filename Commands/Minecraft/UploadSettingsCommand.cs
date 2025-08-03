e13559e12ecacaafd692dffb872347af91b4c3b7
Äkwav <ekwav@coflnet.com>
2025-08-03 14:08:24 +0200

    hook up settings upload fix #303

--- Commands/Minecraft/UploadSettingsCommand.cs
+++ Commands/Minecraft/UploadSettingsCommand.cs
@@ -1,13 +1,24 @@
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Coflnet.Sky.Commands.Shared;
 
 namespace Coflnet.Sky.Commands.MC;
 
 public class UploadSettingsCommand : McCommand
 {
+    private static SettingsUpdater updater = new SettingsUpdater();
     public override async Task Execute(MinecraftSocket socket, string arguments)
     {
         var settings = Convert<Dictionary<string, string>>(arguments);
-        socket.Dialog(db => db.MsgLine($"Received {settings.Count}"));
+        var service = socket.GetService<SettingsService>();
+        foreach (var setting in settings)
+        {
+            var current = await updater.GetCurrentValue(socket, setting.Key);
+            if (current.ToString() != setting.Value)
+            {
+                await updater.Update(socket, setting.Key, setting.Value);
+                socket.Dialog(db => db.MsgLine($"Updated {setting.Key} to {setting.Value}", null, $"From {current}"));
+            }
+        }
     }
 }
