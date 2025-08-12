25474e62cf9e2738d295d59e0a071302d64657cb
Äkwav <ekwav@coflnet.com>
2025-08-12 16:17:11 +0200

    remove pet leveling from attributeflip

--- Commands/State/AttributeFlip.cs
+++ Commands/State/AttributeFlip.cs
@@ -35,7 +35,7 @@ public class AttributeFlipCommand : ReadOnlyListCommand<AttributeFlipCommand.Att
 
     protected override void Format(MinecraftSocket socket, DialogBuilder db, AttributeFlip elem)
     {
-        db.MsgLine($"{McColorCodes.GREEN}{elem.Tag} {McColorCodes.RED}{elem.AuctionPrice} {McColorCodes.GRAY}+{McColorCodes.RED}{elem.EstimatedCraftingCost} {McColorCodes.RESET}to {McColorCodes.AQUA}{socket.FormatPrice(elem.Target)} {McColorCodes.RESET}apply:",
+        db.MsgLine($"{McColorCodes.GREEN}{elem.Tag} {McColorCodes.RED}{socket.FormatPrice(elem.AuctionPrice)} {McColorCodes.GRAY}+{McColorCodes.RED}{socket.FormatPrice(elem.EstimatedCraftingCost)} {McColorCodes.RESET}to {McColorCodes.AQUA}{socket.FormatPrice(elem.Target)} {McColorCodes.RESET}apply:",
                 $"/viewauction {elem.AuctionToBuy}",
                 $"click to open the auction in question\n"
                 + $"{McColorCodes.GRAY}do that before you buy the things to upgrade\n"
@@ -50,11 +50,11 @@ public class AttributeFlipCommand : ReadOnlyListCommand<AttributeFlipCommand.Att
         var service = socket.GetService<IAttributeApi>();
         var raw = await service.ApiAttributeCraftsGetWithHttpInfoAsync();
         var deserialized = JsonConvert.DeserializeObject<List<AttributeFlip>>(raw.RawContent);
-        foreach (var item in deserialized)
+        foreach (var item in deserialized.ToList())
         {
-            if (NBT.IsPet(item.Tag) && item.EndingKey.Tier > item.StartingKey.Tier)
+            if (NBT.IsPet(item.Tag) && (item.EndingKey.Tier > item.StartingKey.Tier || item.StartingKey.Modifiers.FirstOrDefault(k => k.Key == "exp").Value != item.EndingKey.Modifiers.FirstOrDefault(k => k.Key == "exp").Value))
             {
-                item.Ingredients.Add(new() { AttributeName = $"Kat pet upgrade to {item.EndingKey.Tier}" });
+                deserialized.Remove(item); // remove pet exp or kat leveling
             }
         }
         return deserialized;
