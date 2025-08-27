5e4efdd155365e07f263325bf597203ad1e67199
Äkwav <ekwav@coflnet.com>
2025-08-27 12:11:30 +0200

    Filter ahflips

--- Commands/Flipping/AhFlipsCommand.cs
+++ Commands/Flipping/AhFlipsCommand.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
+using Coflnet.Sky.Core;
 using Coflnet.Sky.FlipTracker.Client.Api;
 using Coflnet.Sky.FlipTracker.Client.Model;
 using Coflnet.Sky.ModCommands.Dialogs;
@@ -17,9 +19,12 @@ namespace Coflnet.Sky.Commands.MC;
     "nature of ah flipping")]
 public class AhFlipsCommand : ReadOnlyListCommand<UnsoldFlip>
 {
+    protected override string Title => "(probably) unsold flips";
+    protected override int PageSize => 8;
     protected override void Format(MinecraftSocket socket, DialogBuilder db, UnsoldFlip elem)
     {
-        var converted = JsonConvert.DeserializeObject<Core.LowPricedAuction>(JsonConvert.SerializeObject(elem.Flip));
+        var converted = System.Text.Json.JsonSerializer.Deserialize<Core.LowPricedAuction>(System.Text.Json.JsonSerializer.Serialize(elem.Flip));
+        converted.Auction.FlatenedNBT = elem.Flip.Auction.FlatNbt;
         var formatted = socket.formatProvider.FormatFlip(FlipperService.LowPriceToFlip(converted));
         db.MsgLine(formatted,
             $"/viewauction {elem.Flip.Auction.Uuid}",
@@ -30,9 +35,15 @@ public class AhFlipsCommand : ReadOnlyListCommand<UnsoldFlip>
     {
         if (!await socket.RequirePremium())
             return [];
+        socket.Dialog(db => db.MsgLine("Chcking recently found flips if they are known to be sold", null, "Auctions might still be gone by the time you click on them"));
         var trackerApi = socket.GetService<ITrackerApi>();
-        var unsold = await trackerApi.GetUnsoldFlipsAsync(DateTime.UtcNow.AddMinutes(-1), 30);
-        return unsold;
+        var unsold = await trackerApi.GetUnsoldFlipsAsync(DateTime.UtcNow.AddMinutes(-1.2), 50);
+        var uids = unsold.Select(f => f.Uid).ToArray();
+        using var db = new HypixelContext();
+        var known = db.Auctions.Where(f => uids.Contains(f.UId) && f.End < DateTime.UtcNow).Select(f => f.UId).ToHashSet();
+        unsold = unsold.Where(f => !known.Contains(f.Uid)).ToList();
+        return unsold.OrderByDescending(f => (f.Flip.TargetPrice - f.Flip.Auction.StartingBid) * f.Flip.DailyVolume)
+            .Where(f => f.Flip.Auction.StartingBid < socket.SessionInfo.Purse || socket.SessionInfo.Purse <= 0).ToList();
     }
 
     protected override string GetId(UnsoldFlip elem)
