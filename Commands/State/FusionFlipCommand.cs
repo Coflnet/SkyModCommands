e61fad60ed55246ef9716ad677552503b285293e
Äkwav <ekwav@coflnet.com>
2025-08-29 15:28:54 +0200

    sort fusion flips by profit

--- Commands/State/FusionFlipCommand.cs
+++ Commands/State/FusionFlipCommand.cs
@@ -16,16 +16,16 @@ public class FusionFlipCommand : ReadOnlyListCommand<FuseFlip>
     protected override void Format(MinecraftSocket socket, DialogBuilder db, FuseFlip elem)
     {
         db.MsgLine($"Combine {string.Join(" and ", elem.Inputs.Select(i => $"{McColorCodes.GOLD}{i.Key}{McColorCodes.GRAY}x{i.Value}"))} to {McColorCodes.AQUA}{elem.Output} {McColorCodes.RESET}for {McColorCodes.GOLD}{socket.FormatPrice(elem.OutputValue)}",
-                $"/bz {elem.Output.Replace("SHARD_","")}",
+                $"/bz {elem.Output.Replace("SHARD_", "")}",
                 $"click to open the bz of {elem.Output}\n{McColorCodes.GRAY}to check the price yourself"
                 + $"\n{McColorCodes.GRAY}Volume: {elem.Volume}")
-            .ForEach(elem.Inputs, (db, ing) => db.MsgLine($"{McColorCodes.GRAY}- bz {McColorCodes.RESET}{ing.Key} {McColorCodes.GRAY}x{ing.Value}", "/bz " + ing.Key.Replace("SHARD_",""), "Open the bazaar to place buy order for this item \n(click)"));
+            .ForEach(elem.Inputs, (db, ing) => db.MsgLine($"{McColorCodes.GRAY}- bz {McColorCodes.RESET}{ing.Key} {McColorCodes.GRAY}x{ing.Value}", "/bz " + ing.Key.Replace("SHARD_", ""), "Open the bazaar to place buy order for this item \n(click)"));
     }
 
     protected override async Task<IEnumerable<FuseFlip>> GetElements(MinecraftSocket socket, string val)
     {
         var bazaarFlipApi = socket.GetService<IBazaarFlipperApi>();
-        var fusions = await bazaarFlipApi.FusionGetAsync();
+        var fusions = (await bazaarFlipApi.FusionGetAsync()).OrderByDescending(f => (f.OutputValue - f.InputCost) * f.Volume).ToList();
 
         if (await socket.UserAccountTier() == Shared.AccountTier.NONE)
             foreach (var item in fusions.Take(2))
