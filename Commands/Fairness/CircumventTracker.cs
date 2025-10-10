038af44477ac91f587f1497e02920a94581e01ea
Äkwav <ekwav@coflnet.com>
2025-10-10 13:50:34 +0200

    use bin for challenges

--- Commands/Fairness/CircumventTracker.cs
+++ Commands/Fairness/CircumventTracker.cs
@@ -188,7 +188,7 @@ public class CircumventTracker
     private static async Task<SaveAuction> FindAuction(IMinecraftSocket socket)
     {
         var oldestStart = DateTime.UtcNow - TimeSpan.FromMinutes(1);
-        foreach (var blocked in socket.TopBlocked.Where(b => b.Flip.Auction.Start > oldestStart)
+        foreach (var blocked in socket.TopBlocked.Where(b => b.Flip.Auction.Start > oldestStart && b.Flip.Auction.Bin)
                                     .OrderBy(b => b.Flip.Auction.StartingBid / b.Flip.DailyVolume))
         {
             if (blocked.Flip.Auction.StartingBid > 90_000_000)
