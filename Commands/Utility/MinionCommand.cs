c02615eaef2b87bdeded8e501b5b8c568d2a7678
Äkwav <ekwav@coflnet.com>
2025-12-10 23:24:35 +0100

    Add info on hover to minion list

--- Commands/Utility/MinionCommand.cs
+++ Commands/Utility/MinionCommand.cs
@@ -11,9 +11,22 @@ namespace Coflnet.Sky.Commands.MC;
 public class MinionCommand : ReadOnlyListCommand<MinionService.MinionEffect>
 {
     protected override string Title => "Top Minions by profit";
+    public MinionCommand()
+    {
+        sorters.Add("cost", list => list.OrderByDescending(e => e.craftCostEst));
+        sorters.Add("nbc", list => list.OrderByDescending(e => e.nbcSellPerDay));
+        sorters.Add("cheapest", list => list.OrderBy(e => e.craftCostEst));
+    }
     protected override void Format(MinecraftSocket socket, DialogBuilder db, MinionService.MinionEffect elem)
     {
-        db.MsgLine($" {elem.name} {McColorCodes.RED}{socket.FormatPrice(elem.craftCostEst)}/minion {McColorCodes.GRAY}is {McColorCodes.AQUA}{socket.FormatPrice(elem.profitPerDay)} per day");
+        db.MsgLine($" {elem.name} {McColorCodes.RED}{socket.FormatPrice(elem.craftCostEst)}/minion {McColorCodes.GRAY}is {McColorCodes.AQUA}{socket.FormatPrice(elem.profitPerDay)} per day",
+            "https://hypixel-skyblock.fandom.com/wiki/" + elem.name.Replace(" ", "_"),
+            $"Profit per day: {McColorCodes.AQUA}{socket.FormatPrice(elem.profitPerDay)}\n" +
+            $"Estimated cost to craft: {McColorCodes.RED}{socket.FormatPrice(elem.craftCostEst)}\n" +
+            $"Estimated NPC sell per day: {McColorCodes.GOLD}{socket.FormatPrice(elem.nbcSellPerDay)}\n" +
+            $"Delay per action: {McColorCodes.YELLOW}{elem.minionData.TierDelay.Last()} seconds\n" +
+            $"Products: {McColorCodes.GREEN}{string.Join(", ", elem.minionData.Products.Where(p => p.Tag != null).Select(p => p.Tag))}\n" +
+            $"{McColorCodes.YELLOW}Click to view on the Skyblock Wiki");
     }
 
     protected override async Task<IEnumerable<MinionService.MinionEffect>> GetElements(MinecraftSocket socket, string val)
