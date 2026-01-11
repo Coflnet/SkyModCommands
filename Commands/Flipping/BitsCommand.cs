fa4d989294a6402789355dfe3faa5ba6714f5134
Äkwav <ekwav@coflnet.com>
2026-01-11 13:49:56 +0100

    warp to elizabethon click

--- Commands/Flipping/BitsCommand.cs
+++ Commands/Flipping/BitsCommand.cs
@@ -10,6 +10,7 @@ namespace Coflnet.Sky.Commands.MC;
 public class BitsCommand : ReadOnlyListCommand<BitService.Option>
 {
     protected override string Title => "Bit Options";
+    protected override int PageSize => 10;
 
     protected override async Task<IEnumerable<BitService.Option>> GetElements(MinecraftSocket socket, string val)
     {
@@ -20,7 +21,7 @@ public class BitsCommand : ReadOnlyListCommand<BitService.Option>
     protected override void Format(MinecraftSocket socket, DialogBuilder db, BitService.Option elem)
     {
         db.MsgLine($"{McColorCodes.YELLOW}{elem.Name} {McColorCodes.GOLD}{socket.FormatPrice(elem.CoinsPerBit)} coins per bit",
-            null,
+            "/warp elizabeth",
             $"\n{McColorCodes.GRAY}Sell price: {McColorCodes.YELLOW}~{socket.FormatPrice(elem.CoinsPerBit * elem.TotalBits)} coins\n"
           + $"{McColorCodes.GRAY}Total Bits required: {McColorCodes.YELLOW}{elem.TotalBits}");
     }
@@ -33,7 +34,8 @@ public class BitsCommand : ReadOnlyListCommand<BitService.Option>
         var bestAffordable = elements.Where(e => e.TotalBits <= available).OrderByDescending(e => e.CoinsPerBit).FirstOrDefault();
         if (bestAffordable == null)
             return;
-        db.MsgLine($"Best affordable option is {McColorCodes.YELLOW}{bestAffordable.Name}{McColorCodes.GRAY} for {McColorCodes.YELLOW}{socket.FormatPrice(bestAffordable.CoinsPerBit * bestAffordable.TotalBits)} coins");
+        db.MsgLine($"Best affordable option is {McColorCodes.YELLOW}{bestAffordable.Name}{McColorCodes.GRAY} for {McColorCodes.YELLOW}{socket.FormatPrice(bestAffordable.CoinsPerBit * bestAffordable.TotalBits)} coins",
+        "/warp elizabeth", $"{McColorCodes.YELLOW}Click to warp to Community Shop");
     }
 
     protected override string GetId(BitService.Option elem)
