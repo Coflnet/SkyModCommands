ab192f5d23d3cf40dbc87a6c4ff54b6c2d939db3
Äkwav <ekwav@coflnet.com>
2026-02-11 13:26:06 +0100

    filter empty slots

--- Commands/FullAfVersionAdapter.cs
+++ Commands/FullAfVersionAdapter.cs
@@ -140,7 +140,7 @@ public class FullAfVersionAdapter : AfVersionAdapter
         if (socket.Version.StartsWith("af-2"))
             return;
         inventory ??= await WaitForInventory();
-        var tags = inventory.Select(i => i.Tag).ToHashSet();
+        var tags = inventory.Where(i => i != null && i.Tag != null).Select(i => i.Tag).ToHashSet();
         var bazaarItems = await socket.GetService<Bazaar.Client.Api.IOrderBookApi>().GetOrderBooksAsync(tags.ToList());
         var bazaaritemTags = bazaarItems.Where(b => b.Value.Sell?.Count > 0).Select(b => b.Key).ToHashSet();
         var amounts = inventory.Where(i => bazaaritemTags.Contains(i.Tag)).GroupBy(i => i.Tag).ToDictionary(g => g.Key, g => (g.Sum(i => i.Count), g.First().ItemName));
