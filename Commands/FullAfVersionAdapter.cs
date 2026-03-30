dfe7bdb427353c2bfafa6006c7faf0b88dd68333
Äkwav <ekwav@coflnet.com>
2026-03-30 23:56:39 +0200

    update enchanted book search vlaues

--- Commands/FullAfVersionAdapter.cs
+++ Commands/FullAfVersionAdapter.cs
@@ -642,6 +642,12 @@ public partial class FullAfVersionAdapter : AfVersionAdapter
     /// <param name="amount">The amount to order</param>
     public void SendBazaarOrderRecommendation(string itemTag, string itemName, bool isSell, double price, int amount)
     {
+        if(itemName == "Enchanted Book")
+        {
+            // for enchanted books, include enchantments in the name for better clarity
+            itemName = BazaarUtils.GetSearchValue(itemTag, itemTag.Replace("ENCHANTMENT_", "").Replace("_", " ").ToLower());
+            Activity.Current?.Log($"Updated item name for enchanted book recommendation: {itemName}");
+        }
         socket.Send(Response.Create("placeOrder", new
         {
             itemName = itemName,
